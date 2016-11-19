03e4d0d14b9c076a0c4e914462c462197f0a8933
Haniel11 <harenjaegger@gmail.com>
2016-11-19 17:10:43 -0600

    version 2

--- Demo/app/src/main/java/uca/demo/app/demo/MainActivity.java
+++ Demo/app/src/main/java/uca/demo/app/demo/MainActivity.java
@@ -1,13 +1,108 @@
 package uca.demo.app.demo;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.stephentuso.welcome.WelcomeHelper;
 
 public class MainActivity extends AppCompatActivity {
+    WelcomeHelper welcomeScreen;
+    private Button create;
+    private EditText cardNumer;
+    private DatabaseReference myRef;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Mensaje de Bienvenida
+
+        welcomeScreen = new WelcomeHelper(this, MyWelcomeActivity.class);
+        welcomeScreen.show(savedInstanceState);
+//Base de datos con Firebase
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+       myRef = database.getReference("message");
+        //myRef.setValue("Hello, World!");
+        //
+        initCreateNumber();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        welcomeScreen.onSaveInstanceState(outState);
+    }
+
+    private void initCreateNumber() {
+        create = (Button) findViewById(R.id.create);
+        cardNumer = (EditText) findViewById(R.id.CardNumber);
+
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (cardNumer.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(),"No HAY Texto",Toast.LENGTH_LONG).show();
+                }
+                else{
+                    myRef.setValue(cardNumer.getText().toString());
+                }
+            }
+        });
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
---
+++ Demo/app/src/main/java/uca/demo/app/demo/MyWelcomeActivity.java
@@ -0,0 +1,39 @@
+package uca.demo.app.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.stephentuso.welcome.BasicPage;
+import com.stephentuso.welcome.TitlePage;
+import com.stephentuso.welcome.WelcomeConfiguration;
+import com.stephentuso.welcome.WelcomeActivity;
+
+
+public class MyWelcomeActivity extends WelcomeActivity{
+
+    /*@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_welcome);
+    }*/
+    @Override
+    protected WelcomeConfiguration configuration() {
+        return new WelcomeConfiguration.Builder(this)
+                .defaultBackgroundColor(R.color.background)
+                .page(new TitlePage(R.drawable.ic_ok,
+                        "Title")
+                )
+                .page(new BasicPage(R.drawable.ic_ok,
+                        "Header",
+                        "More text.")
+                        .background(R.color.red_background)
+                )
+                .page(new BasicPage(R.drawable.ic_ok,
+                        "Lorem ipsum",
+                        "dolor sit amet.")
+                )
+                .swipeToDismiss(true)
+                .build();
+    }
+
+}
